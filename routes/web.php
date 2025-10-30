5e0603c022717778fed17ecbc20c7b8f5e238268
shiroe <ican.notfound@gmail.com>
2025-10-30 11:20:56 +0800

    eps3 belajar livewire

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
+use App\Livewire\Users;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/counter', Counter::class);
+Route::get('/users', function(){
+    return view('users');
+});
